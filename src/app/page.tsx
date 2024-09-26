a126ab87fface5a829b2b49aa6fe9b6902e3219c
Adam <adamwasylak1@gmail.com>
2024-09-26 19:59:49 +0200

    add features to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
+import { BackgroundBeams } from '@/components/ui/background-beams'
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import Link from 'next/link'
 
 const Home = async () => {
 	return (
-		<div className='grid w-full h-screen grid-cols-2 col'>
+		<div className='z-10 grid w-full h-screen grid-cols-2 col'>
 			<div className='flex flex-col items-center justify-center gap-7'>
-				<h2 className='text-3xl font-bold'>CleanNotes</h2>
-				<p>Collect everything in one place!</p>
-				<div className='flex gap-4 '>
+				<h2 className='text-5xl font-bold'>CleanNotes</h2>
+				<p className='text-xl text-gray-300 max-w-[500px]'>
+					Collect all your thoughts, tasks, and ideas in one place! Whether it is a to-do list, a creative idea, or a
+					reminder, keep everything organized and easily accessible. No more scattered notes—have it all at your
+					fingertips. 
+				
+				</p>
+				<div className='flex gap-4 mt-5 '>
 					<Link href='/notes'>
 						<Button className='text-black bg-yellow-400 border border-black hover:bg-yellow-400/85'>Get Started</Button>
 					</Link>
@@ -17,6 +23,9 @@ const Home = async () => {
 							Login
 						</Button>
 					</Link>
+					<div className='-z-10'>
+						<BackgroundBeams />
+					</div>
 				</div>
 			</div>
 			<div className='flex items-center justify-center'>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from "tailwindcss"
 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 const config = {
   darkMode: ["class"],
   content: [
@@ -34,7 +39,18 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), addVariablesForColors],
 } satisfies Config
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
 
 export default config
