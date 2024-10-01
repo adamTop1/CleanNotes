1824bd650d295ac59f346b6902c093b20853ff00
Adam <adamwasylak1@gmail.com>
2024-10-01 18:46:41 +0200

    add ui for signup page

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -6,20 +6,20 @@ import { useActionState } from 'react'
 import { Button } from './ui/button'
 
 export default function LoginForm() {
-	const [errorMessage, formAction, isPending] = useActionState(authenticate, undefined)
+	// const [errorMessage, formAction, isPending] = useActionState(authenticate, undefined)
 
 	return (
-		<form action={formAction} className='space-y-3'>
-			<div className='flex-1 px-6 pt-8 pb-4 rounded-lg bg-gray-50'>
+		<form  className='space-y-3 '>
+			<div className='flex-1 px-6 pt-8 pb-4 rounded-lg'>
 				<h1 className={`mb-3 text-2xl`}>Please log in to continue.</h1>
 				<div className='w-full'>
 					<div>
-						<label className='block mt-5 mb-3 text-xs font-medium text-gray-900' htmlFor='email'>
+						<label className='block mt-5 mb-3 text-xs font-medium text-gray-400' htmlFor='email'>
 							Email
 						</label>
 						<div className='relative'>
 							<input
-								className='peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500'
+								className='block w-full rounded-md border border-gray-200 py-[9px] pl-1 text-sm outline-2 placeholder:text-gray-400'
 								id='email'
 								type='email'
 								name='email'
@@ -29,12 +29,12 @@ export default function LoginForm() {
 						</div>
 					</div>
 					<div className='mt-4'>
-						<label className='block mt-5 mb-3 text-xs font-medium text-gray-900' htmlFor='password'>
+						<label className='block mt-5 mb-3 text-xs font-medium text-gray-500' htmlFor='password'>
 							Password
 						</label>
 						<div className='relative'>
 							<input
-								className='peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500'
+								className='block w-full rounded-md border border-gray-200 py-[9px] pl-1 text-sm outline-2 placeholder:text-gray-500'
 								id='password'
 								type='password'
 								name='password'
@@ -42,15 +42,15 @@ export default function LoginForm() {
 								required
 								minLength={6}
 							/>
-							<KeyIcon className='pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
+							
 						</div>
 					</div>
 				</div>
-				<Button className='w-full mt-4' aria-disabled={isPending}>
+				<Button className='w-full mt-4 bg-zinc-800/70 hover:bg-zinc-800'>
 					Log in <ArrowRightIcon className='w-5 h-5 ml-auto text-gray-50' />
 				</Button>
 				<div className='flex items-end h-8 space-x-1' aria-live='polite' aria-atomic='true'>
-					{errorMessage && <>{errorMessage}</>}
+				
 				</div>
 			</div>
 		</form>
