8c51833b63500bf58ed9116549938e41b2dd5637
Adam <adamwasylak1@gmail.com>
2024-10-02 21:53:23 +0200

    add logo and paragraph in signin and signup page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,15 +1,27 @@
 import { signInAction } from '@/actions/users'
 import LoginForm from '@/components/LoginForm'
+import Link from 'next/link'
 import React from 'react'
 
-
 const SignIn = () => {
-  return (
-   	<div className='flex flex-col items-center justify-center w-full min-h-screen bg-zinc-900'> 
-        <h2 className='pb-1 my-20 text-5xl border-b-2 border-yellow-300'>Sign In</h2>
-        <LoginForm actionFn={signInAction} signText='sign in' />
-    </div>
-  )
+	return (
+		<>
+			<h1 className='absolute z-10 mx-auto mt-10 text-4xl font-bold text-white border-b border-yellow-300 top-5 left-10 '>
+				<Link href='/'>CleanNotes</Link>
+			</h1>
+
+			<div className='flex flex-col items-center justify-center w-full min-h-screen bg-zinc-900'>
+				<h2 className='pb-1 my-20 text-5xl border-b-2 border-yellow-300 '>Sign In</h2>
+				<LoginForm actionFn={signInAction} signText='sign in' />
+				<p>
+					If you do not have an account, please{' '}
+					<Link href='/signup' className='text-yellow-300'>
+						sign up.
+					</Link>
+				</p>
+			</div>
+		</>
+	)
 }
 
 export default SignIn
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,14 +1,26 @@
 import LoginForm from '@/components/LoginForm'
 import React from 'react'
 import { signUpAction } from '@/actions/users'
+import Link from 'next/link'
 
 const SignUp = () => {
-  return (
-   	<div className='flex flex-col items-center justify-center w-full min-h-screen bg-zinc-900'> 
-        <h2 className='pb-1 my-20 text-5xl border-b-2 border-yellow-300'>Sign Up</h2>
-        <LoginForm actionFn={signUpAction} signText='sign up' />
-    </div>
-  )
+	return (
+		<>
+			<h1 className='absolute z-10 mx-auto mt-10 text-4xl font-bold text-white border-b border-yellow-300 top-5 left-10 '>
+				<Link href='/'>CleanNotes</Link>
+			</h1>
+			<div className='flex flex-col items-center justify-center w-full min-h-screen bg-zinc-900'>
+				<h2 className='pb-1 my-20 text-5xl border-b-2 border-yellow-300'>Sign Up</h2>
+				<LoginForm actionFn={signUpAction} signText='sign up' />
+				<p>
+					If you have an account, please{' '}
+					<Link href='/signin' className='text-yellow-300'>
+						sign in.
+					</Link>
+				</p>
+			</div>
+		</>
+	)
 }
 
 export default SignUp
